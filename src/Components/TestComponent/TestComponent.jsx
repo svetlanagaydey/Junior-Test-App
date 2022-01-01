1f64f63b3298d5e19bf1a132a7f2a6bfb0cb2217
svetlanagaydey <lana84@ukr.net>
2022-01-01 22:37:20 +0200

    Warning message corrected

--- src/Components/TestComponent/TestComponent.jsx
+++ src/Components/TestComponent/TestComponent.jsx
@@ -1,50 +1,18 @@
 import React, { useState } from 'react';
 import './TestComponent.css';
-
-const testsList = [
-	{
-		testName: 'Test Name',
-		testId: 123,
-		questions: [
-			{
-				questionId: 1000,
-				questionText: 'What is the Capital of Great Britain',
-				options: ['London', 'Paris', 'Kyiv', 'Berlin'],
-				writeOptionInex: [0],
-			}
-		]
-	},
-	{
-		testName: 'Test Name',
-		testId: 124,
-		questions: [
-			{
-				questionId: 1001,
-				questionText: 'Сhoose the second and fourth option',
-				options: ['First', 'Second', 'Third', 'Fourth'],
-				writeOptionInex: [1, 3],
-			}
-		]
-	}, 
-	{
-		testName: 'Test Name',
-		testId: 125,
-		questions: [
-			{
-				questionId: 1002,
-				questionText: 'What is no food in the list?',
-				options: ['Tomato', 'Fish', 'Coca-Cola', 'Cucumber'],
-				writeOptionInex: [2],
-			}
-		]
-	},
-]
+import TestsList from './TestList';
 
 const TestComponent = () => {
 	const [currentQuestion, setCurrentQuestion] = useState(0);
-	const toNext = () => {
+	const [answers, setAnswer] = useState();
+	async function toNext()  {
 		setCurrentQuestion(currentQuestion + 1);
 	}
+	const setCurrentAnswer = (e) => {
+		setAnswer(e.target.id);	
+		console.log(answers);
+	}
+
 
 	return (
 		<div className='test-page'>
@@ -56,30 +24,32 @@ const TestComponent = () => {
 				</div>
 				
 				<div className='test-card'>
-					<h3>{testsList[currentQuestion].testName}</h3>
-					{testsList[currentQuestion].questions.map((quest) => {
+					<h3>{TestsList[currentQuestion].testName}</h3>
+					{TestsList[currentQuestion].questions.map((quest) => {
 						return (
 							<div key={quest.questionId}>
 								<h4>{quest.questionText}</h4>
 								<ul className='questionsList'>
-									{quest.options.map((answer, index) => {
-										
-										if (quest.writeOptionInex.length===1) {
+									{quest.options.filter(() => quest.writeOptionInex.length===1)
+									.map((answer, index) => {
 											return (
 												<li key={index} className='answer'>
-													<input type="radio" id={index} name='radio'/>
+													<input type="radio" id={index} name='radio' onChange={setCurrentAnswer}/>
 													<label htmlFor={index}>{answer}</label>
 												</li>
 											)
-										} else if (quest.writeOptionInex.length>1) {
+										})
+									}
+									{quest.options.filter(() => quest.writeOptionInex.length>1)
+									.map((answer, index) => {
 											return (
 												<li key={index} className='answer'>
-													<input type="checkbox" id={index}/>
-													<label htmlFor={index}>{answer}</label>
+												<input type="checkbox" id={index}/>
+												<label htmlFor={index}>{answer}</label>
 												</li>
 											)
-										}
-									})}
+										})
+									}
 								</ul>
 							</div>
 						)
---
+++ src/Components/TestComponent/TestList.js
@@ -0,0 +1,40 @@
+const TestsList = [
+	{
+		testName: 'Test Name',
+		testId: 123,
+		questions: [
+			{
+				questionId: 1000,
+				questionText: 'What is the Capital of Great Britain',
+				options: ['London', 'Paris', 'Kyiv', 'Berlin'],
+				writeOptionInex: [0],
+			}
+		]
+	},
+	{
+		testName: 'Test Name',
+		testId: 124,
+		questions: [
+			{
+				questionId: 1001,
+				questionText: 'Сhoose the second and fourth option',
+				options: ['First', 'Second', 'Third', 'Fourth'],
+				writeOptionInex: [1, 3],
+			}
+		]
+	}, 
+	{
+		testName: 'Test Name',
+		testId: 125,
+		questions: [
+			{
+				questionId: 1002,
+				questionText: 'What is no food in the list?',
+				options: ['Tomato', 'Fish', 'Coca-Cola', 'Cucumber'],
+				writeOptionInex: [2],
+			}
+		]
+	},
+]
+
+export default TestsList;
