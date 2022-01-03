a5841813ad34fee57fdc512ec009d4a252d32766
svetlanagaydey <lana84@ukr.net>
2022-01-04 00:25:03 +0200

    question section completed

--- src/Components/AddQuestionPage/AddQuestionPage.jsx
+++ src/Components/AddQuestionPage/AddQuestionPage.jsx
@@ -1,55 +1,94 @@
-import React, { useEffect } from 'react';
-import FormQuestion from '../FormQuestion/FormQuestion';
-import UsersQuestionsList from './UsersQuestionsList';
-import {useState} from 'react'
+
+import React from 'react';
+import './AddQuestionPage.css';
 
 
-const AddQuestionPage = () => {
-    const[userQuestions, setUserQuestion] = useState([]);
-    const[curQuest, setCurQuest] = useState('');
-    useEffect(() => {
-        console.log('delete useEfect')
-    }, [userQuestions])
+class AddQuestionPage extends React.Component {
 
-    const handleChange = (e) => {
-        setCurQuest(e.target.value);
-    }
-    const addQuestion = (e) => {
-        e.preventDefault()
-        if(curQuest!=='') {
-            let temp = userQuestions;
-            temp.push(curQuest);
-            setUserQuestion(temp);
-            setCurQuest('')
-            // setCurQuest([]);
-            // setUserQuestion('');
-        }
-    }
-    function deleteQuestion(ind) {
-        const questions = userQuestions;
-        questions.splice(ind, 1);
-        console.log(questions)
-        setUserQuestion(questions);
-        console.log(userQuestions)
-    }   
-    const printList = userQuestions.length
-        ?
-        (userQuestions.map((question, index) => {
-            return <UsersQuestionsList question={question} key={index} index={index} deleteQuestion={deleteQuestion}  />
-        }))
-        : <span>It is now questions from users yet!)))</span>;
+	state={
+		act: 0,
+		index: '',
+		datas: []
+	}
 
-     
+  componentDidMount(){
+    this.refs.question.focus();
+  }
 
+  submitQuestion = (e) => {
+    e.preventDefault();
+    console.log('try');
+
+    let datas = this.state.datas;
+    let question = this.refs.question.value;
+    let answer = this.refs.answer.value;
+
+    if(this.state.act === 0){   //new
+      let data = {
+        question, answer
+      }
+      datas.push(data);
+    } else {                      //update
+      let index = this.state.index;
+      datas[index].question = question;
+      datas[index].answer = answer;
+    }    
+
+    this.setState({
+      datas: datas,
+      act: 0,
+    });
+
+    this.refs.myForm.reset();
+    this.refs.question.focus();
+  }
+
+  removeQuestion = (i) => {
+    let datas = this.state.datas;
+    datas.splice(i,1);
+    this.setState({
+      datas: datas,
+    });
+
+    this.refs.myForm.reset();
+    this.refs.question.focus();
+  }
+
+  editQuestion = (i) => {
+    let data = this.state.datas[i];
+    this.refs.question.value = data.question;
+    this.refs.answer.value = data.answer;
+
+    this.setState({
+      act: 1,
+      index: i
+    });
+
+    this.refs.question.focus();
+  }  
+
+  render() {
+    let datas = this.state.datas;
     return (
+      <div className="add_containter">
+        <h2>Submit you question here!</h2>
+        <form ref="myForm" className="myForm">
+          <input type="text" ref="question" placeholder="Your question" className="formField" />
+          <input type="text" ref="answer" placeholder="Answer to the question" className="formField" />
+          <button onClick={(e)=>this.submitQuestion(e)} className="myButton">SUBMIT </button>
+        </form>
         <div>
-            <h2>Live your question here:</h2>
-            <FormQuestion handleChange={handleChange}
-                addQuestion={addQuestion}
-                curQuest={curQuest}
-             />
-            {printList}
+          {datas.map((data, i) =>
+            <li key={i} className="myList">
+              {i+1}. {data.question}, {data.answer}
+              <button onClick={()=>this.removeQuestion(i)} className="myListButton">DELETE </button>
+              <button onClick={()=>this.editQuestion(i)} className="myListButton">EDIT </button>
+            </li>
+          )}
         </div>
-    )
+      </div>
+    );
+  }
 }
+
 export default AddQuestionPage;
